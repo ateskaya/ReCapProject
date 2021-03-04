240d18a4be397e0c13a8e2c48d9474531bb600dc
ensar <ensar@DESKTOP-PJ52DT8>
2021-03-05 00:47:46 +0300

    10.gün 4.ödev

--- DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
+++ DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
@@ -12,6 +12,9 @@ namespace DataAccess.Concrete
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ReCapProject;Trusted_Connection=true");
         }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<Car> Cars2 { get; set; }
         public DbSet<Color> Colors { get; set; }
         public DbSet<Brand> Brands { get; set; }
