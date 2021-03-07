5ed6e003b1a69d4d8aac30efb48eec11b290b036
ensar <ensar@DESKTOP-PJ52DT8>
2021-03-08 00:04:04 +0300

    11. ödev1 Sonu

--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class ColorDal : EfEntityRepositoryBase<Color, ReCapProjectContext>, IColorDal
+    public class EfColorDal : EfEntityRepositoryBase<Color, ReCapProjectContext>, IColorDal
     {
         
     }
